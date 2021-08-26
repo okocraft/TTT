7b7a7f2a6f9a894a4f1588a1d6df300b565728d5
LazyGon <lazygon.mc@gmail.com>
2021-08-26 18:46:06 +0900

    feat: add aliases to tab completion

--- src/main/java/net/okocraft/ttt/command/TTTCommand.java
+++ src/main/java/net/okocraft/ttt/command/TTTCommand.java
@@ -6,12 +6,16 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.Set;
 
 public class TTTCommand extends AbstractCommand implements CommandExecutor, TabCompleter {
 
@@ -56,10 +60,19 @@ public class TTTCommand extends AbstractCommand implements CommandExecutor, TabC
         }
 
         if (args.length == 1) {
-            return subCommands.stream()
-                    .map(AbstractCommand::getName)
-                    .filter(cmdName -> cmdName.equalsIgnoreCase(args[0]))
-                    .toList();
+            return StringUtil.copyPartialMatches(
+                    args[0],
+                    subCommands.stream()
+                            .map(command -> {
+                                Set<String> canditates = new HashSet<>();
+                                canditates.add(command.getName());
+                                canditates.addAll(command.getAliases());
+                                return canditates;
+                            })
+                            .flatMap(Set::stream)
+                            .toList(),
+                    new ArrayList<>()
+            );
         }
 
         return search(args[0])
